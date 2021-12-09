dd69188a1332dbfedbc8fd2372b444fecd25fb87
OFIR REGEV <ofirmeidan1998@gmail.com>
2021-12-09 12:27:49 +0200

    commit

--- src/Ex2.java
+++ src/Ex2.java
@@ -58,7 +58,7 @@ public class Ex2 {
 
     public static void main(String[] args) throws IOException {
 //        String file = args[0];
-       String filename = "C:\\Users\\avi44\\IdeaProjects\\Ex2_06_12\\src\\data\\G1.json";
+       String filename = "C:\\Users\\avi44\\IdeaProjects\\Ex2_06_12\\src\\data\\G3.json";
         getGrapgAlgo(filename);
         runGUI(filename);
 
